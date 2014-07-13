ee9fdca6329b7c89d8710cd38dcb1df1dcd30606
Kevin Wierman <kwierman@gmail.com>
2014-07-13 15:46:12 -0400

    Init Commit

---
+++ plotview/src/main/java/com/kwierman/triplot/plotview/PlotData.java
@@ -0,0 +1,9 @@
+package com.kwierman.triplot.plotview;
+
+/**
+ * Created by kwierman on 7/13/14.
+ */
+public interface PlotData {
+    public float GetX();//require float
+    public float GetY();
+}
---
+++ plotview/src/main/java/com/kwierman/triplot/plotview/PlotView.java
@@ -0,0 +1,21 @@
+package com.kwierman.triplot.plotview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by kwierman on 7/13/14.
+ */
+public class PlotView extends View {
+
+
+    public  PlotView(Context context){
+        super(context);
+    }
+
+    public PlotView(Context context, AttributeSet attrs){
+        super(context, attrs);
+    }
+
+}
